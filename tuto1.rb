d02b18c1c70cac23f03e3dbb28c68e06045229fa
LAPTOP-CAR9KD80\wp <hninsuhlaing2282020@gmail.com>
2022-04-21 10:20:36 +0630

    add tuto1 file

---
+++ tuto1.rb
@@ -0,0 +1,24 @@
+heigth = 5
+width = heigth *2 - 1
+
+for i in 0..heigth
+	for j in 0..width + 1
+		if j >= heigth - i && j <= heigth + i
+			print "*"
+		else
+			print " "
+			
+		end
+	end
+	puts
+end
+for i in (heigth - 1).downto(0)
+	for j in (width + 1).downto(0)
+		if j >= heigth - i && j <= heigth + i
+			print "*"
+		else
+			print " "
+		end
+	end
+	puts
+end
